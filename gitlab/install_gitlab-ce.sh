2773be86b2859e6ee8a667056b531f620ef214b2
rimas <rmocius@gmail.com>
2017-05-15 13:33:14 +0100

    Update to support SPC domain

--- gitlab/install_gitlab-ce.sh
+++ gitlab/install_gitlab-ce.sh
@@ -2,6 +2,6 @@
 helm repo up
 
 helm upgrade gitlab-ce gitlab-ce --namespace gitlab \
-  --set externalHostname="rimusz.xyz",runnerRegistrationToken="49uf3mzs1qdz5vgo" \
+  --set instanceId="spcm0eiut2",env=".staging",runnerRegistrationToken="49uf3mzs1qdz5vgo" \
   --set gitlabRootPassword="pass1234",runners.namespace="gitlab" \
   -i
