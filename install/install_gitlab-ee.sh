b0cac0b26d919f854afca41a89c63c54f3933677
rimas <rmocius@gmail.com>
2017-06-13 11:38:29 +0100

    add install instructions and rename gitlab chart

---
+++ install/install_gitlab-ee.sh
@@ -0,0 +1,26 @@
+#
+helm repo up
+
+#
+helm upgrade nginx-ingress spc/nginx-ingress --namespace nginx-ingress \
+  --set tcp.22="gitlab/gitlab-ce-gitlab-ce:22" -i
+
+#
+helm upgrade kube-lego spc/kube-lego --namespace kube-lego \
+  --set config.LEGO_EMAIL="vendors@stackpointcloud.com" \
+  --set config.LEGO_URL="https://acme-v01.api.letsencrypt.org/directory" \
+  -i
+
+# replace 'instanceId' with a actual kube cluster name
+# for production do not set 'env'
+helm upgrade gitlab-ee spc/gitlab --namespace gitlab \
+  --set image="gitlab/gitlab-ce:9.2.2-ee.0" \
+  --set instanceId="spcm0eiut2",env=".staging",runnerRegistrationToken="49uf3mzs1qdz5vgo" \
+  --set gitlabRootPassword="pass1234",runners.namespace="gitlab" \
+  -i
+
+#
+helm upgrade gitlab-runner spc/gitlab-runner --namespace gitlab \
+  --set gitlabUrl=http://gitlab-ee-gitlab:8005/,runnerRegistrationToken="49uf3mzs1qdz5vgo" \
+  --set runners.privileged="true" \
+  -i
---
+++ install/install_kube-lego.sh
@@ -0,0 +1,7 @@
+#
+helm repo up
+
+helm upgrade kube-lego spc/kube-lego --namespace kube-lego \
+  --set config.LEGO_EMAIL="vendors@stackpointcloud.com" \
+  --set config.LEGO_URL="https://acme-v01.api.letsencrypt.org/directory" \
+  -i
