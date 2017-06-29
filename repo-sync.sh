86168b027df5ad41acc9c9bcde35fce91de8eef6
Nate Franzen <nfranzen+github@gmail.com>
2017-06-29 14:58:06 -0700

    minor script fix: avoid stray files in chart directory

--- repo-sync.sh
+++ repo-sync.sh
@@ -9,10 +9,13 @@ function gen_packages() {
   mkdir -p temp
   for d in stable/*
   do
+   if [[ -d $d ]]
+   then
       # Will generate a helm package per chart in a folder
       echo $d
       helm package $d
       mv *.tgz temp/
+    fi
   done
 }
 
