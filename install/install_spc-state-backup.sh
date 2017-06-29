4cc6654c872c59f87f722912c8a188b07a974b3b
rimas <rmocius@gmail.com>
2017-06-29 18:02:42 +0100

    bump docker image, other small improvements

--- install/install_spc-state-backup.sh
+++ install/install_spc-state-backup.sh
@@ -1,18 +1,18 @@
 # install for the first time
 # AWS S3
-helm upgrade --install kube-backup spc/spc-state-backup --namespace stackpoint-system \
+helm upgrade --install spc-kube-backup spc/spc-state-backup --namespace stackpoint-system \
     --set schedule="*/50 * * * *",aws.enabled="true" \
     --set aws.accessKeyId="UNF37N93MG374B27",aws.secretAccessKey="AvgjbYndf9TMF8Y3F3J993TMTJ2309T" \
     --set aws.bucket="my-kube-state-backup-bucket",aws.region="eu-west-2" \
     --set jobCleanup.enabled="true" # set it only for k8s v1.5
 
 # GCS
-helm upgrade --install kube-backup spc/spc-state-backup --namespace stackpoint-system \
+helm upgrade --install spc-kube-backup spc/spc-state-backup --namespace stackpoint-system \
     --set schedule="*/50 * * * *",gcs.enabled="true",gcs.projectId="project-123" \
     --set gcs.serviceAccountKey="$(cat /path/to/service-account.json | base64 -w 0)" \
-    --set gcs.bucket="my-kube-state-backup",gcs.region="eu" \
+    --set gcs.bucket="my-kube-state-backup",gcs.region="europe-west1" \
     --set jobCleanup.enabled="true" # set it only for k8s v1.5
 
 # run upgrades e.g. for schedule or docker image change
-helm upgrade kube-backup spc/spc-state-backup --reuse-values --set schedule="*/30 * * * *"
-helm upgrade kube-backup spc/spc-state-backup --reuse-values --set imageTag="0.1.8"
+helm upgrade spc-kube-backup spc/spc-state-backup --reuse-values --set schedule="*/30 * * * *"
+helm upgrade spc-kube-backup spc/spc-state-backup --reuse-values --set imageTag="0.1.11"
