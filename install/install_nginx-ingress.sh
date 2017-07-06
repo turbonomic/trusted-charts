6a52cc56e968d0b8014daea61517fb511592c6aa
Rimas Mocevicius <rimusz@users.noreply.github.com>
2017-07-06 11:00:14 +0100

    Update install_nginx-ingress.sh

--- install/install_nginx-ingress.sh
+++ install/install_nginx-ingress.sh
@@ -2,4 +2,4 @@
 helm repo up
 
 helm upgrade nginx-ingress spc/nginx-ingress --namespace nginx-ingress \
-  --set tcp.22="gitlab/gitlab-ce-gitlab-ce:22" -i
+  --set tcp.22="gitlab/gitlab-ce-gitlab:22" -i
